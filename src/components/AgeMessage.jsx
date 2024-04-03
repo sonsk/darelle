0ff2d029192201c69d591a66647defd77e142248
sonsk <sonfacksteve15@gmail.com>
2024-04-03 22:13:16 +0100

    second

--- src/components/AgeMessage.jsx
+++ src/components/AgeMessage.jsx
@@ -23,10 +23,10 @@ export default function AgeMessage(props) {
             <span className="border-solid border-2 border-blue-200 rounded-full p-2 px-4">I'm {props.age}  </span>
             <span></span>
             </p>
-            <p className="relative w-10/12 mx-auto top-20 text-center  text-3xl mt-5 font-bold">
+            <p className="relative w-10/12 mx-auto top-20 text-center  text-3xl mt-5 sm:text-xl font-bold">
             It's my birthday
             </p>
-            <p className="relative w-10/12 mx-auto top-20 text-center  text-4xl mt-5 font-light">
+            <p className="relative w-10/12 mx-auto top-20 text-center  text-4xl mt-5 sm:text-xl font-light">
             May the dreams I'm working on come true
             </p>
         </div>
@@ -36,14 +36,14 @@ export default function AgeMessage(props) {
             <span className="border-solid border-2 border-red-200 rounded-full p-2 px-4">I'm {props.age}  </span>
             <span></span>
             </p>
-            <p className="relative w-10/12 mx-auto top-20 text-center  text-2xl mt-5 font-light">
+            <p className="relative w-10/12 mx-auto top-20 text-center  text-2xl mt-5 sm:text-xl font-light">
             Not yet my birthday
             </p>
-            <p className="relative w-10/12 mx-auto top-20 text-center  text-2xl mt-5 font-light">
+            <p className="relative w-10/12 mx-auto top-20 text-center  text-2xl mt-5 sm:text-xl font-light">
                 All I have to do now is work to achieve what I've set out to do.
             </p>
             <p className="relative w-8/12 mx-auto top-20 text-center  text-4xl mt-5 font-light">
-            <img src="angry.png" className="angry" alt="angry"/>
+            <img src="angry.png" className="angry hidden hidden-on-custom" alt="angry"/>
             </p>
         </div>
         
